de264aa677f97ffccb22adb9e70ee5c93896820f
Ankit_1066 <ankit9270791066@gmail.com>
2022-03-17 15:48:39 +0530

    initial commit

--- app/src/main/java/com/example/needmedz/OcrActivity.java
+++ app/src/main/java/com/example/needmedz/OcrActivity.java
@@ -1,25 +1,37 @@
 package com.example.needmedz;
 
-import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
-
-import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.github.dhaval2404.imagepicker.ImagePicker;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.Text;
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.TextRecognizer;
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
+
+import java.io.IOException;
 
 public class OcrActivity extends AppCompatActivity {
 
@@ -27,6 +39,8 @@ public class OcrActivity extends AppCompatActivity {
     Button generateText ;
     Button next;
     ImageView i ;
+    EditText resultTV ;
+    private Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +48,7 @@ public class OcrActivity extends AppCompatActivity {
         setContentView(R.layout.activity_ocr);
 
         logout = findViewById(R.id.button2);
+        resultTV = findViewById(R.id.editTextTextMultiLine);
         generateText = findViewById(R.id.button);
         next = findViewById(R.id.button3);
         i = findViewById(R.id.imageView2);
@@ -50,13 +65,13 @@ public class OcrActivity extends AppCompatActivity {
         }
     });
 
-
-    generateText.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            Toast.makeText(OcrActivity.this, " Backend is Yet to be established ", Toast.LENGTH_SHORT).show();
-        }
-    });
+        generateText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(OcrActivity.this, " Backend is Yet to be established ", Toast.LENGTH_SHORT).show();
+                detect();
+            }
+        });
 
     next.setOnClickListener(new View.OnClickListener() {
         @Override
@@ -76,62 +91,62 @@ public class OcrActivity extends AppCompatActivity {
                     .compress(1024)			//Final image size will be less than 1 MB(Optional)
                     .maxResultSize(1080, 1080)	//Final image resolution will be less than 1080 x 1080(Optional)
                     .start();
-//            boolean pick = true;
-//            if(pick == true ){
-//                if(!checkCameraPermission()){
-//                    requestCameraPermission();
-//
-//                }else PickImage();
-//            } else {
-//                if(!checkStoragePermission()){
-//                    requestStoragePermission();
-//
-//                }else PickImage();
-//            }
         }
     });
 
     }
-
-//    private void PickImage() {
-//
-////        CropImage.activity()
-////                .setGuidelines(CropImageView.Guidelines.ON)
-////                .start(this);
-//
-//    }
-//
-//    @RequiresApi(api = Build.VERSION_CODES.M)
-//    private void requestStoragePermission() {
-//        requestPermissions(new String[]{ Manifest.permission.WRITE_EXTERNAL_STORAGE }, 100);
-//    }
-//
-//    @RequiresApi(api = Build.VERSION_CODES.M)
-//    private void requestCameraPermission() {
-//        requestPermissions(new String[]{ Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE }, 100);
-//    }
-//
-//    private boolean checkStoragePermission() {
-//        boolean res2 = ContextCompat.checkSelfPermission(this , Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
-//        return res2;
-//    }
-//
-//    private boolean checkCameraPermission() {
-//        boolean res1 = ContextCompat.checkSelfPermission(this , Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
-//        boolean res2 = ContextCompat.checkSelfPermission(this , Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
-//        return  res1 && res2;
-//    }
 
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == Activity.RESULT_OK) {
             Uri uri = data.getData();
             i.setImageURI(uri);
+            try {
+                 bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
+            } catch (IOException e) {
+                e.printStackTrace();
+
+
+            }
         }
         else {
             Toast.makeText(this, "Task Cancelled", Toast.LENGTH_SHORT).show();
         }
     }
+
+    private void detect() {
+        InputImage image = InputImage.fromBitmap(bitmap,0);
+        TextRecognizer recognizer  = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
+        Task<Text> result = recognizer.process(image).addOnSuccessListener(new OnSuccessListener<Text>() {
+            @Override
+            public void onSuccess(Text text) {
+                 StringBuilder result = new StringBuilder();
+                 for(Text.TextBlock block : text.getTextBlocks()){
+                    String BlockText = block.getText();
+                     Point[]  blockCornerPoint = block.getCornerPoints();
+                     Rect blockframe = block.getBoundingBox();
+                     for (Text.Line line : block.getLines()){
+                         String lineTExt = line.getText();
+                         Point[] lineCorneerPoint = line.getCornerPoints();
+                         Rect linrect = line.getBoundingBox();
+                         for (Text.Element element: line.getElements()){
+                             String elementText = element.getText();
+                             result.append(elementText);
+                         }
+                         resultTV.setText( block.toString());
+                     }
+                 }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(OcrActivity.this, "Fail to Detect text from image", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+
 
 }
 
